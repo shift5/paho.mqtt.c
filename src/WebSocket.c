a020d4998e7915edc7cb87fe3227b3c312442834
Mike Detwiler <det@shift5.io>
2019-12-10 21:14:31 -0500

    fix MinGW and OSX builds

--- src/WebSocket.c
+++ src/WebSocket.c
@@ -29,6 +29,11 @@
 #include "SocketBuffer.h"
 #include "StackTrace.h"
 
+#ifdef __MINGW32__
+#define htonll __builtin_bswap64
+#define ntohll __builtin_bswap64
+#endif
+
 #if defined(__linux__)
 #  include <endian.h>
 #elif defined(__APPLE__)
